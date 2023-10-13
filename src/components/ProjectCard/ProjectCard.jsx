7ec93a9c443eea124820e51b059f51d33d25b35f
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-10-13 16:57:59 -0600

    adjustings the links for the projects

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const ProjectCard = ({ project }) => {
   return (
-    <>
     <Card style={{ margin: '20px', maxWidth: '345px' }}>
       <CardMedia
+        className='projectImage'
         component="img"
-        height="140"
+        height="350"
         image={project.imageURL}
         alt={project.title}
       />
@@ -15,12 +17,23 @@ const ProjectCard = ({ project }) => {
         <Typography gutterBottom variant="h5" component="div">
           {project.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" align="justify">
           {project.description}
         </Typography>
+        <Box display="flex" justifyContent="center" alignItems="center" marginTop="16px" gap="16px">
+          {project.projectURL && 
+            <a href={project.projectURL} target="_blank" rel="noopener noreferrer">
+              <LinkIcon />
+            </a>
+          }
+          {project.gitURL && 
+            <a href={project.gitURL} target="_blank" rel="noopener noreferrer">
+              <GitHubIcon />
+            </a>
+          }
+        </Box>
       </CardContent>
     </Card>
-    </>
   );
 };
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -6,20 +6,44 @@ import ProjectCard from '../../components/ProjectCard/ProjectCard';
 const Projects = () => {
   const projects = [
     {
-      title: 'Proyecto 1',
-      description: 'Descripción del Proyecto 1',
-      imageURL: 'https://image.shutterstock.com/image-photo/3d-image-neon-abstract-260nw-2258892637.jpg'
+      title: 'Unibook-Pro. for Jeblush',
+      description: `Developed a full-stack RESTful application for a renowned beauty salon in Mexico City, designed to streamline their service reservations. React | Mongo | JavaScript | HTML | CSS`,
+      imageURL: 'https://i.ibb.co/0sjXmkC/unibook-pro.jpg',
+      gitURL: 'https://github.com/',
+      projectURL: 'https://unibookpro-app.netlify.app/'
     },
     {
-      title: 'Proyecto 2',
-      description: 'Descripción del Proyecto 2',
-      imageURL: 'https://image.shutterstock.com/image-photo/neon-3d-image-robot-face-260nw-2271841123.jpg'
+      title: 'LOL-Wiki',
+      description: `This project is a dynamic app designed to tap into the League of Legends API. Developed using Bootstrap and Handlebars, the app presents a user-friendly interface that brings detailed game insights to the players`,
+      imageURL: 'https://i.ibb.co/WHjnHX8/lolwiki.jpg',
+      gitURL: 'https://github.com/',
+      projectURL: ''
     },
-    // ... Agrega más proyectos según lo necesites
+    {
+      title: 'Vampire VS Zombies',
+      description: `Navigate a world where a vampire avoids zombies using canvas-rendered visuals. Consume pumpkins to transform and turn the undead into your prey. A captivating game crafted with HTML, CSS, and JavaScript.`,
+      imageURL: 'https://i.ibb.co/frFMJ5g/vampirevszombies.jpg',
+      gitURL: 'https://github.com/Andresfrla/vampires-vs-zombies',
+      projectURL: 'https://andresfrla.github.io/vampires-vs-zombies/'
+    },
+    {
+      title: 'Tic Tac Toe',
+      description: `Harness the power of React in this modern rendition of the classic Tic Tac Toe game. Experience smooth gameplay, interactive UI, and dynamic state management as you strategize your way to three in a row. A timeless game with a contemporary twist.`,
+      imageURL: 'https://i.ibb.co/n0wfn8R/tictactoe.jpg',
+      gitURL: 'https://github.com/Andresfrla/Tik-Tak-Toe-React',
+      projectURL: ''
+    },
+    {
+      title: 'Clone with bootstrap',
+      description: `Dive into a meticulously crafted clone, mirroring the essence of the original, all powered by Bootstrap's responsive framework. Experience familiar designs paired with seamless responsiveness. Bootstrap | HTML | CSS`,
+      imageURL: 'https://i.ibb.co/QYg5Dz5/cloning.jpg',
+      gitURL: 'https://github.com/Andresfrla/Tik-Tak-Toe-React',
+      projectURL: ''
+    }
   ];
 
   return (
-    <div style={{ padding: '40px' }}>
+    <div className="projectsContainer">
       <Typography variant="h4" gutterBottom>
         My Projects
       </Typography>
