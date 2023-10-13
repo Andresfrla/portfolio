cc07f11081c18402411e3552f4561bfccf02d124
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-10-13 17:25:19 -0600

    adjusting the home responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,22 +9,29 @@ import './style.css';
 function Home() {
   return (
     <div className="homeContainer">
-          <Container className={'intro'} id='home' fluid>
-          <h1>&sdot; <Typed
-            strings={[
-                      " Hello! I'm Andres ",
-                      " Web developer ",
-                      " Business Administrator ",
-                      ]}
-          typeSpeed={60}
-          backSpeed={50}
-          loop /> &sdot;</h1>
+        <Container className={'intro'} id='home' fluid>
+            <h1>&sdot; 
+                <Typed
+                    strings={[
+                        " Hello! I'm Andres ",
+                        " Web developer ",
+                        " Business Administrator ",
+                    ]}
+                    typeSpeed={60}
+                    backSpeed={50}
+                    loop 
+                /> 
+            &sdot;</h1>
         </Container>
-      <Container>
-        <About/>
-        <Projects/>
-        <ContactMe/>
-      </Container>
+        <Container className="about">
+            <About/>
+        </Container>
+        <Container className="projects">
+            <Projects/>
+        </Container>
+        <Container className="contactMe">
+            <ContactMe/>
+        </Container>
     </div>
   );
 }
